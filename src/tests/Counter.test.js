345131658fe9ebd2393b6a238a72c8bac2ab076f
Gbigott <92874470+Gbigott@users.noreply.github.com>
2024-04-09 21:04:36 -0400

    finish assignment

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,41 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen,fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
 
-beforeEach(() => {
-  // Render the Counter component here
-})
+// beforeEach(() => {
+//   // Render the Counter component here
+// })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const welcomeMessage = screen.getByText(/counter/i);
+  expect(welcomeMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+
+  const{getByTestId} = render(<Counter />);
+  const welcomeMessage = Number(getByTestId("count").textContent);
+  expect(welcomeMessage).toEqual(0);
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const{getByTestId,getByRole} = render(<Counter />);
+  const detect_increment = getByRole("button",{name:"+"});
+  const initial_val = Number(getByTestId("count").textContent);
+  expect( initial_val).toEqual(0);
+  fireEvent.click(detect_increment);
+  const final_val = Number(getByTestId("count").textContent);
+  
+  expect(final_val).toEqual(1);
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const{getByTestId,getByRole} = render(<Counter />);
+  const detect_decrement = getByRole("button",{name:"-"});
+  const initial_val = Number(getByTestId("count").textContent);
+  expect( initial_val).toEqual(0);
+  fireEvent.click(detect_decrement);
+  const final_val = Number(getByTestId("count").textContent);
+  
+  expect(final_val).toEqual(-1);
 });
